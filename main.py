44d2222b5f5dccd5728821f62b5056dac8d2c8b7
Your Name <you@example.com>
2024-05-27 15:19:25 +0300

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import types
 from aiogram import Bot, Dispatcher, executor, types
 from config import TELEGRAM_TOKEN
+from keyboards.keyboards import get_keyboard_1, get_keyboard_2
 
 bot = Bot(token = TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
@@ -18,7 +19,23 @@ async def set_commands(bot: Bot):
 
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
-    await message.answer("Привет!")
+    await message.answer("Привет!", reply_markup= get_keyboard_1())
+
+@dp.message_handler(lambda message: message.text == "Отправь фото кота.")
+async def button_1_click(message: types.Message):
+    await bot.send_photo(message.chat.id, photo="https://img.goodfon.com/original/1600x1200/e/e6/zhivotnoe-kot-bolshie-glaza.jpg", caption= "Вот тебе кот.")
+
+@dp.message_handler(lambda message: message.text == "Перейти на след. клавиатуру.")
+async def button_2_click(message: types.Message):
+    await message.answer("Тут ты можешь попросить бота отправить фото собаки.", reply_markup=get_keyboard_2())
+
+@dp.message_handler(lambda message: message.text == "Отправь фото собаки.")
+async def button_3_click(message: types.Message):
+    await bot.send_photo(message.chat.id, photo="https://w.forfun.com/fetch/2b/2b370a552f767dce12a4e612e2711c76.jpeg", caption= "Вот тебе собака.")
+
+@dp.message_handler(lambda message: message.text == "Вернуться на прошлую клавиатуру.")
+async def button_4_click(message: types.Message):
+    await message.answer("Тут ты можешь попросить бота отправить фото кота.", reply_markup=get_keyboard_1())
 
 @dp.message_handler(commands="help")
 async def start(message: types.Message):
